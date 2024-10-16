b7a5fbabc0cf100e91ba56a129d7e5e469390fb7
runjhun singh <112868347+runjhun809@users.noreply.github.com>
2024-10-16 18:05:00 +0000

    operators

---
+++ practice1/datatypes.js
@@ -0,0 +1,8 @@
+//primitive
+
+/* 7 types : string,Number,Boolean,Null,undefined,symbol,BigInt
+
+refrence (non primitive)
+ array,objects,functions */
+
+ 
---
+++ practice1/operators.js
@@ -0,0 +1,20 @@
+/*let str1="hello"
+let str2="sona"
+let str3=str1+str2
+console.log(str3);*/
+//hellosona
+
+console.log("1"+2);
+
+console.log("1"+2+2);
+
+console.log(+true);
+
+console.log(+"");
+
+let gameCounter=100;
+gameCounter++;
+console.log(gameCounter);
+
+
+
